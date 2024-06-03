843e45e4d08ee27a4d20cf001e4f794c013b3104
TimotheeHrl <tharleaux@gmail.com>
2024-06-03 17:16:07 +0200

    LUT-26864 : Add a blog post editor advanced options

--- src/java/fr/paris/lutece/plugins/blog/web/adminDashboard/BlogAdminDashboardComponent.java
+++ src/java/fr/paris/lutece/plugins/blog/web/adminDashboard/BlogAdminDashboardComponent.java
@@ -21,6 +21,7 @@ import javax.servlet.http.HttpServletRequest;
 public class BlogAdminDashboardComponent extends AdminDashboardComponent
 {
     private static final long serialVersionUID = 3045411044102177294L;
+    private static final String TEMPLATE_MANAGE_EDITOR_CONFIG = "admin/plugins/blog/dashboard/manage_editor_config.html";
     private static final String MARK_NUMBER_MANDATORY_TAGS = "number_mandatory_tags";
     private static final String TEMPLATE_MANDATORY_TAGS = "admin/plugins/blog/dashboard/modify_mandatory_blog_tags.html";
     private static final String ACTION_UPDATE_MANDATORY_TAG_NUMBER = "updateMandatoryTagNumber";
@@ -48,6 +49,8 @@ public class BlogAdminDashboardComponent extends AdminDashboardComponent
         model.put( PARAMETER_MAX_PUBLICATION_DATE_VALUE, maxPublicationDate );
         HtmlTemplate template2 = AppTemplateService.getTemplate( TEMPLATE_MANAGE_MAX_PUBLICATION_DATE, user.getLocale( ), model );
         strTemplate += template2.getHtml( );
+        HtmlTemplate template3 = AppTemplateService.getTemplate( TEMPLATE_MANAGE_EDITOR_CONFIG, user.getLocale( ) );
+        strTemplate += template3.getHtml( );
         return strTemplate;
     }
     @Action( ACTION_UPDATE_MANDATORY_TAG_NUMBER )
