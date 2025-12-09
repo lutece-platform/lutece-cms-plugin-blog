044840131a3300075729a5648d23626c940d6aac
Jeremy Deloche <jeremy.deloche@atos.com>
2025-12-09 08:50:02 +0100

    LUT-31438 : When changing the content type in a blog post properties it is not updated in database

--- src/java/fr/paris/lutece/plugins/blog/web/BlogJspBean.java
+++ src/java/fr/paris/lutece/plugins/blog/web/BlogJspBean.java
@@ -1600,6 +1600,7 @@ public class BlogJspBean extends ManageBlogJspBean
                 contType.setIdContentType( Integer.parseInt( strContentTypeId ) );
 
                 content.setContentType( contType );
+                DocContentHome.update( content );
                 break;
             }
         }
